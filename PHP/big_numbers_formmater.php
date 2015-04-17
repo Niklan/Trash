1457296b707257cc88f9df928a630a46c3fdd879
Niklan <niklanrus@gmail.com>
2015-04-17 15:11:07 +0500

    Big numbers formats.

---
+++ PHP/big_numbers_formmater.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @file
+ * Shortiner for big numbers. Translate numbers like 1000 to 1k, 1 000 000 to 1M.
+ */
+function format_big_number($number) {
+  $tokens = array(
+    1000000 => 'M',
+    1000 => 'K',
+    1 => ''
+  );
+
+  foreach ($tokens as $unit => $text) {
+    if ($number < $unit) {
+      continue;
+    }
+    return floatval(number_format($number / $unit, 1)) . $text;
+  }
+}
+
+/**
+ * @example
+ */
+echo format_big_number(100); // 100
+echo format_big_number(1000); // 1K
+echo format_big_number(20500); // 20.5K
+echo format_big_number(1500000); // 1.5M
+
